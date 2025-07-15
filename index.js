5123b0b3d6b6bc444bcc6c0e65ea9b43a6309738
Elizabeth <elizabethcommon12@gmail.com>
2025-07-14 21:10:11 -0400

    changes

--- index.js
+++ index.js
@@ -5,14 +5,20 @@
 
 function contact(event){
     event.preventDefault();
-    emailjs
-        .sendForm(
-            'service_hgre0ni',
-            'template_4m21yr6',
-            event.target,
-            'pU-TiVS8GqxtDsVdb'
-        ).then(() => {
-            console.log('this worked1');
-        })
+//    emailjs
+//        .sendForm(
+//            'service_hgre0ni',
+//            'template_4m21yr6',
+//            event.target,
+//            'pU-TiVS8GqxtDsVdb'  
+//      ).then(() => {
+//            console.log('this worked1');
+//        })
+const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
+setTimeout (() => {
+    console.log('it worked 1');
+}, 500);
 
 }
+
