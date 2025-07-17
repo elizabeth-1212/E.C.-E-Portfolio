26ad444a88709a44c81d09ed940264333e8f591c
Elizabeth <elizabethcommon12@gmail.com>
2025-07-16 22:50:27 -0400

    changes4

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+let contrastToggle = false;
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if(contrastToggle) {
+        document.body.classList += " dark-theme";
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
 
 function contact(event){
     event.preventDefault();
